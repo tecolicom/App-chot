7e4a49de2a15d12ceb07fa856b972b7ec42f770a
Kazumasa Utashiro <kaz@utashiro.com>
2026-01-24 14:21:16 +0900

    Checking in changes prior to tagging of version 0.12.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c5d3ecd..1e78d06 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for "lms" command
     
     {{$NEXT}}
     
    +0.12 2026-01-24T05:21:15Z
    +
    +    - Search Homebrew libexec paths for Perl modules.
    +    - Add GitHub Actions workflow to create releases on tag push.
    +
     0.11 2026-01-24T04:45:36Z
     
         - Add Homebrew wrapper resolution to show actual scripts.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ lms - Let Me See command
 
 # VERSION
 
-Version 0.11
+Version 0.12
 
 # SYNOPSIS
 
--- lib/App/lms.pm
+++ lib/App/lms.pm
@@ -1,6 +1,6 @@
 package App::lms;
 
-our $VERSION = "0.11";
+our $VERSION = "0.12";
 
 use v5.14;
 use warnings;
