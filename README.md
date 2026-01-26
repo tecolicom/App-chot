b665f31af870e9beeb0c5c47feccb87b9b98bcc3
Kazumasa Utashiro <kaz@utashiro.com>
2026-01-26 19:25:16 +0900

    Checking in changes prior to tagging of version 0.16.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 75cc130..7cdafd1 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for "lms" command
     
     {{$NEXT}}
     
    +0.16 2026-01-26T10:25:15Z
    +
    +    - Deduplicate Perl modules with identical content using MD5 hash.
    +
     0.15 2026-01-25T11:26:13Z
     
         - Add --py option as shortcut for --type Python.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ lms - Let Me See command
 
 # VERSION
 
-Version 0.15
+Version 0.16
 
 # SYNOPSIS
 
--- lib/App/lms.pm
+++ lib/App/lms.pm
@@ -1,6 +1,6 @@
 package App::lms;
 
-our $VERSION = "0.15";
+our $VERSION = "0.16";
 
 use v5.14;
 use warnings;
