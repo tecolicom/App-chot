6e915cbf71dadefc56e83e99fd634620a4c22d48
Kazumasa Utashiro <kaz@utashiro.com>
2026-02-02 10:19:06 +0900

    Checking in changes prior to tagging of version 0.21.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 812599d..05dba42 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for "lms" command
     
     {{$NEXT}}
     
    +0.21 2026-02-02T01:19:05Z
    +
    +    - Add --bat-theme option for automatic BAT_THEME selection based on terminal background
    +
     0.20 2026-02-01T11:28:13Z
     
         - Support Linuxbrew path resolution in Homebrew wrapper detection

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ lms - Let Me See command
 
 # VERSION
 
-Version 0.20
+Version 0.21
 
 # SYNOPSIS
 
--- lib/App/lms.pm
+++ lib/App/lms.pm
@@ -1,6 +1,6 @@
 package App::lms;
 
-our $VERSION = "0.20";
+our $VERSION = "0.21";
 
 use v5.14;
 use warnings;
